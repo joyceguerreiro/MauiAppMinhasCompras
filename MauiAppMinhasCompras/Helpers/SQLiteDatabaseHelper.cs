fc69ef21c50eb37427a3acc5d8929270af365480
jkgsoares <joyce.soares@numenit.com>
2025-03-03 03:13:51 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,52 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+        public SQLiteDatabaseHelper(string path) 
+        { 
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        //Insert
+        public Task<int> Insert(Produto p) 
+        {
+            return _conn.InsertAsync(p);
+        }
+        
+        //Update
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
+        }
+
+        //Delete
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        //GetAll
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        //Search
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+    }
+}
