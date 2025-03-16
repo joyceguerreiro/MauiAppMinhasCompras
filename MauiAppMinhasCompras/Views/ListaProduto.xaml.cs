17cdcb488d16d3fe3326a119c2d8d628d6767ffe
jkgsoares <joyce.soares@numenit.com>
2025-03-16 03:58:42 -0300

    Alterações para Agenda 04: lista de visualização, barra de pesquisa, soma e exclusão.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,12 +1,27 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
+
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
 	}
 
+    protected async override void OnAppearing()
+    {
+        List<Produto> tpm = await App.Db.GetAll();
+
+        tpm.ForEach(i => lista.Add(i));
+    }
+    
+    //Botão Adicionar
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
         try
@@ -19,4 +34,42 @@ public partial class ListaProduto : ContentPage
             DisplayAlert("Ops", ex.Message, "OK");
         }
     }
+
+    //Botão Buscar
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tpm = await App.Db.Search(q);
+
+        tpm.ForEach(i => lista.Add(i));
+    }
+
+    //Botão Somar
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK"); 
+    }
+
+    //Botão Remover
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        var menuItem = (MenuItem)sender;
+        var produto = (Produto)menuItem.BindingContext; 
+
+        if (produto != null)
+        {
+            lista.Remove(produto); 
+            lst_produtos.ItemsSource = null; 
+            lst_produtos.ItemsSource = lista;
+        }
+
+    }
+
 }
